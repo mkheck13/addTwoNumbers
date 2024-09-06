69c6d6734f9e054711bcf2950b3a108ac0a714fa
mkheck13 <mkheck13@gmail.com>
2024-09-06 09:05:31 -0700

    add two numbers push

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Clear();
+
+Console.WriteLine("num1");
+
+string? num1 = Console.ReadLine();
+
+Console.WriteLine("num2");
+
+string? num2 = Console.ReadLine();
+
+int num1Con = Convert.ToInt32(num1);
+int num2Con = Convert.ToInt32(num2);
+int sum = num1Con + num2Con;
+
+Console.WriteLine($"so {num1Con} plus {num2Con} equals {sum}");
+
---
+++ README.md
@@ -0,0 +1,14 @@
+# Add Two Numbers
+
+**Description**: Create a console project that prompts the user to enter two numbers. The console then adds the two numbers and 
+returns the sum.
+
+**Name**: Michael Heckerman
+
+**Date**: 9/6/24
+
+### Peer Review:
+
+**Name**:
+
+**Comments**:
